8732d42cfd59ff535413870dce2cf27bbbf65ef8
profvanessaborges <borges.oliveira.vanessa@escola.pr.gov.br>
2025-11-19 10:18:29 -0300

    promt atual ok

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let texto = document.querySelector("#texto");
 
 let nome = prompt("Qual o seu nome?");
 if(nome == null){
-    texto.textContent = "Seja bemvindo(a)!";    
+    texto.textContent = "Seja bem-vindo(a)!";    
 }
 else{
     texto.textContent = nome;
 }
-
-" =
